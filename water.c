ea1a3c43f226d2ed7269eab5e3f03a2713678f80
0x0Eddinee <0x0bal@gmail.com>
2015-12-29 21:09:06 +0100

    water

--- water.c
+++ water.c
@@ -1,23 +1,12 @@
 #include "stdio.h"
+#include "cs50.h"
+
 int main(void){
 
-
-	while(1){	
-
-	printf("How long was your shower? ");
-	int time;
-	scanf("%i", &time);	
-
-	if (time > 0){
-		int bottles = (time*1.5*128)/16;
-		printf("Bottles: %i\n", bottles);
-		}
-
-	else {
-                printf("Retry:\n");
-		
-	        }
-	}
+	printf("Minutes:  ");
+	int time = GetInt();	
+	int bottles = (time*1.5*128)/16;
+	printf("Bottles: %i\n", bottles);
 
 
 	return 0;
