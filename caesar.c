1bffdc4b457a45ebf7fc15256a9ca760486d1311
0x0Eddinee <0x0bal@gmail.com>
2016-01-04 20:52:20 +0100

    documented some things

--- caesar.c
+++ caesar.c
@@ -1,3 +1,8 @@
+//ROT13 || Caesar Cipher
+//It takes an argument when running the program
+
+
+
 #include <stdio.h>
 #include <cs50.h>
 #include <stdlib.h>
@@ -15,9 +20,9 @@ int main(int argc, string argv[]){
         return 1;
     }
     
+        
     int k = atoi(argv[1]);
-
-   
+              
     printf("What would you like to encrypt?\n");
     printf("Text: ");
     string txt = GetString();  
@@ -26,6 +31,8 @@ int main(int argc, string argv[]){
     encrypt(txt,k); 
     
     printf("\n");
+    
+    
     return 0;
 }
 
@@ -36,24 +43,20 @@ void encrypt(string s, int c){
     for(int i = 0, n = strlen(s); i < n; i++ ){
 
 
-        if (s[i] == ' '){
- 
-           printf(" ");
-
-        }else if (islower(s[i])){
-    
+        if (islower(s[i])){
+            //Lowercase
             int a = s[i];
             int b = (a - 97 + c) % 26;
             printf("%c", b + 97);        
         
         }else if (isupper(s[i])){
-
+            //Uppercase
             int a = s[i];
             int b = (a - 65 + c) % 26;
             printf("%c", b + 65);
 
         }else{
-            
+            //If the character isn't alphabetic
             printf("%c", s[i]);        
         }
 
